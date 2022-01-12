25aee677ccee70be8addec99a52c50e7e1d9ec0f
Muhammad Arif Furqon <furqonzt99@gmail.com>
2022-01-12 22:28:11 +0700

    update dsn

--- util/driver.go
+++ util/driver.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"fmt"
 	"layerarch/config"
 	"layerarch/entities"
 
@@ -11,13 +10,15 @@ import (
 
 func InitDB(config *config.AppConfig) *gorm.DB {
 
-	conn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", 
-		config.Database.Username,
-		config.Database.Password,
-		config.Database.Host,
-		config.Database.Port,
-		config.Database.Name,
-	)
+	// conn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", 
+	// 	config.Database.Username,
+	// 	config.Database.Password,
+	// 	config.Database.Host,
+	// 	config.Database.Port,
+	// 	config.Database.Name,
+	// )
+
+	conn := config.Database.Username + ":" + config.Database.Password + "@tcp(" + config.Database.Host + ":" + config.Database.Port + ")/" + config.Database.Name + "?parseTime=true&loc=Asia%2FJakarta&charset=utf8mb4&collation=utf8mb4_unicode_ci"
 
 	db, err := gorm.Open(mysql.Open(conn), &gorm.Config{})
 
